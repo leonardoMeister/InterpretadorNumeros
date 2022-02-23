0d15043859e6583519880be4ff4103fadb75d139
Leonardo Margoti Meister <leozinhomm1@gmail.com>
2022-02-23 14:08:58 -0300

    Add project files.

---
+++ Imterpretador2022.ConsoleApp/Numero.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Imterpretador2022.ConsoleApp
+{
+    public class Numero
+    {
+        private string parte1;
+        private string parte2;
+        private string parte3;
+        private int numero;
+
+        public Numero(string parte1, string parte2, string parte3)
+        {
+            Parte1 = parte1;
+            Parte2 = parte2;
+            Parte3 = parte3;
+            numero = 0;
+        }
+        public int PegarNumero()
+        {
+            if (parte2 == "  | " && parte1 == " _  " && parte3 == " _|_") numero = 1;
+            else if (parte1 == " __ " && parte2 == " __|" && parte3 == "|__ ") numero = 2;
+            else if (parte1 == " __ " && parte2 == " __|" && parte3 == " __|") numero = 3;
+            else if (parte1 == "    " && parte2 == "|__|" && parte3 == "   |") numero = 4;
+            else if (parte1 == " __ " && parte2 == "|__ " && parte3 == " __|") numero = 5;
+            else if (parte1 == " __ " && parte2 == "|__ " && parte3 == "|__|") numero = 6;
+            else if (parte1 == "__  " && parte2 == "  | " && parte3 == "  | ") numero = 7;
+            else if (parte2 == parte3) numero = 8;
+            else if (parte1 == " __ " && parte2 == "|__|" && parte3 == " __|") numero = 9;
+            else if (parte1 == " __ " && parte2 == "|  |" && parte3 == "|__|") numero = 0;
+
+            return numero;
+        }
+        private void CarregarEscrita()
+        {
+            if (numero == 1)
+            {
+                parte2 = "  | "; parte1 = " _  "; parte3 = " _|_";
+            }
+            else if (numero == 2)
+            {
+                parte1 = " __ "; parte2 = " __|"; parte3 = "|__ ";
+            }
+            else if (numero == 3)
+            {
+                parte1 = " __ "; parte2 = " __|"; parte3 = " __|";
+            }
+            else if (numero == 4)
+            {
+                parte1 = "    "; parte2 = "|__|"; parte3 = "   |";
+            }
+            else if (numero == 5)
+            {
+                parte1 = " __ "; parte2 = "|__ "; parte3 = " __|";
+            }
+            else if (numero == 6)
+            {
+                parte1 = " __ "; parte2 = "|__ "; parte3 = "|__|";
+            }
+            else if (numero == 7)
+            {
+                parte1 = "__  "; parte2 = "  | "; parte3 = "  | ";
+            }
+            else if (numero == 8)
+            {
+                parte1 = " __ "; parte2 = "|__|"; parte3 = "|__|";
+            }
+            else if (numero == 9)
+            {
+                parte1 = " __ "; parte2 = "|__|"; parte3 = " __|";
+            }
+            else if (numero == 0)
+            {
+                parte1 = " __ "; parte2 = "|  |"; parte3 = "|__|";
+            }
+        }
+
+        public string RetornarEscrita()
+        {
+            if (!(parte1 != null && parte1 != "")) if (numero > 0 && numero < 10) CarregarEscrita();
+
+            string valor = "";
+            valor += parte1 + "\n";
+            valor += parte2 + "\n";
+            valor += parte3;
+
+            return valor;
+        }
+
+        public string Parte1 { get => parte1; set => parte1 = value; }
+        public string Parte2 { get => parte2; set => parte2 = value; }
+        public string Parte3 { get => parte3; set => parte3 = value; }
+        public int NumeroObj { get => numero; set => numero = value; }
+    }
+}
---
+++ Imterpretador2022.ConsoleApp/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Imterpretador2022.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string letras = " _   __  __      __  __ __   __  __  __ \n" +
+                            "  |  __| __||__||__ |__   | |__||__||  |\n" +
+                            " _|_|__  __|   | __||__|  | |__| __||__|\n" +
+                            "                                        \n" +
+                            " _   __  __      __  __ __   __  __ \n" +
+                            "  |  __| __||__||__ |__   | |__||__|\n" +
+                            " _|_|__  __|   | __||__|  | |__| __|\n" +
+                            "                                        \n" +
+                            " _   __  __      __  __ __   __ \n" +
+                            "  |  __| __||__||__ |__   | |__|\n" +
+                            " _|_|__  __|   | __||__|  | |__|\n" +
+                            "                                        \n" +
+                            " _   __  __      __  __ __   __  __  __ \n" +
+                            "  |  __| __||__||__ |__   | |__||__||  |\n" +
+                            " _|_|__  __|   | __||__|  | |__| __||__|\n";
+
+
+            StringReader reader = new StringReader(letras);
+            List<string> listaLinhas = new List<string>();
+            List<int> listaColunasPorLinhas = new List<int>();
+            List<Numero> listaNumeros = new List<Numero>();
+            int contadorLinhas = 0;
+            int auxContFalhas = 0;
+
+            //pegando os valores e passando para lista de manipulação do sistema
+            while (reader.Peek() != -1)
+            {
+                contadorLinhas++;
+                auxContFalhas++;
+
+                if (auxContFalhas == 4)
+                {
+                    auxContFalhas = 0;
+                    reader.ReadLine();
+                    contadorLinhas--;
+                    continue;
+                }
+                listaLinhas.Add(reader.ReadLine());
+            }
+
+            //Completando Variaveis restantes
+            for (int x = 0; x < listaLinhas.Count / 3; x++) listaColunasPorLinhas.Add(listaLinhas[(((x) + 1) * 3) - 1].Length / 4);
+            contadorLinhas = contadorLinhas / 3;
+
+            #region Mostruario de valores de variaveis
+            /*
+            Console.WriteLine(contadorLinhas);
+            for (int x = 0; x < listaColunasPorLinhas.Count; x++)
+            {
+                Console.WriteLine(listaColunasPorLinhas[x]);
+            }
+            for (int x = 0; x < listaLinhas.Count; x++)
+            {
+                Console.WriteLine(listaLinhas[x]);
+            }*/
+            #endregion
+
+
+            //ATRIBUINDO PARTES PARA FORMAR O OBJETO CONTENDO NUMERO
+            for (int x = 0; x < contadorLinhas; x++)
+            {
+
+                for (int z = 0; z < listaLinhas[((x) * 3)].Length; z += 4)
+                {
+                    string parte1 = "", parte2 = "", parte3 = "";
+
+                    parte1 += listaLinhas[((x) * 3)].Substring(z, 4);
+                    parte2 += listaLinhas[((x) * 3) + 1].Substring(z, 4);
+                    parte3 += listaLinhas[((x) * 3) + 2].Substring(z, 4);
+                    listaNumeros.Add(new Numero(parte1, parte2, parte3));
+                }
+            }
+
+            //IMPRIMINDO OS NUMEROS 
+            int contadorPosicoes = 0;
+            for (int x = 0; x < contadorLinhas; x++)
+            {
+                for (int z = 0; z < listaColunasPorLinhas[x]; z++)
+                {
+                    Console.Write(listaNumeros[(contadorPosicoes)].PegarNumero());
+                    contadorPosicoes++;
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
